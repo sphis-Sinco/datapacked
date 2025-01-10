dd54573bb3235296d8d208ae0d4c29ea97462f84
IdealistCat <mattallan.h@gmail.com>
2025-01-09 21:56:35 -0600

    FileManager changes to adapt to new folder system + datapacks folder

--- source/FileManager.hx
+++ source/FileManager.hx
@@ -5,18 +5,35 @@ class FileManager
 	public static var STATE_FOLDER:String = 'default';
 	public static var SOUND_EXT:String = 'wav';
 
+	public static function getPath(path:String, PATH_TYPE:PathTypes):String
+	{
+		if (PATH_TYPE == PACKS)
+			return getPackPath('game/$path');
+
+		return getAssetPath('game/$path');
+	}
+
 	public static function getAssetPath(path:String):String
 		return 'assets/$path';
 
-	public static function getAssetFile(file:String):String
-		return getAssetPath('$STATE_FOLDER/$file');
+	public static function getPackPath(path:String):String
+		return 'datapacks/$path';
 
-	public static function getDataFile(file:String):String
-		return getAssetFile('data/$file');
+	public static function getAssetFile(file:String, PATH_TYPE:PathTypes):String
+		return getPath('$STATE_FOLDER/$file', PATH_TYPE);
 
-	public static function getImageFile(file:String):String
-		return getAssetFile('images/$file.png');
+	public static function getDataFile(file:String, PATH_TYPE:PathTypes):String
+		return getPath('data/$file', PATH_TYPE);
 
-	public static function getSoundFile(file:String):String
-		return getAssetFile('$file.$SOUND_EXT');
+	public static function getImageFile(file:String, PATH_TYPE:PathTypes):String
+		return getPath('images/$file.png', PATH_TYPE);
+
+	public static function getSoundFile(file:String, PATH_TYPE:PathTypes):String
+		return getPath('$file.$SOUND_EXT', PATH_TYPE);
+}
+
+enum abstract PathTypes(String) from String to String
+{
+	public var ASSETS:String;
+	public var PACKS:String;
 }
